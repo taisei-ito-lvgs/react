9476352a7fc24f833c85125c9e5da6d3868c87a9
taisei.ito <taisei.ito@leverages.jp>
2024-03-27 13:48:03 +0900

    memodayo

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LikeButton />
+      </header>
+    </div>
+  );
+}
+
+function LikeButton(){
+  return<span>いいねボタン予定地</span>
+}
+
+export default App;
